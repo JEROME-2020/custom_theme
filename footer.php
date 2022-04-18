96c2ddc443594e8826d621fd5ccd60162ba236f4
JEROME-2020 <valdezjerome40@gmail.com>
2022-04-17 20:05:55 -0700

    This is Jerome custom theme

---
+++ footer.php
@@ -0,0 +1,12 @@
+<footer class="container-fluid bg-dark pb-3 pt-5">
+    <div class="container yellow-100">
+        <div class="row">
+            <div class="col"><h2 class="text-white">1</h2></div>
+            <div class="col"><h2 class="text-white">2</h2></div>
+            <div class="col"><h2 class="text-white">3</h2></div>
+        </div>
+    </div>
+</footer> 
+<?php wp_footer();?>
+</body>
+</html>
